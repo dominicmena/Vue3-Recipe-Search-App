27b7d05b7abf5aa2a4d1d1b4ebf2545aeb833c68
dominicmena <mena.dominic@gmail.com>
2023-06-17 22:36:42 -0500

    rendered object from meals in browser

---
+++ vuejs-search-meals/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+{{meals}}
+
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../src/store/index';
+
+
+const meals = computed(() => store.state.meals)
+
+</script>
