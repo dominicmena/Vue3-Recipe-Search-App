6be389b71b11340b90f713a337d125fe19732c2a
dominicmena <mena.dominic@gmail.com>
2023-06-19 23:25:13 -0500

    returning data in by-name view

--- vuejs-search-meals/src/views/MealsByName.vue
+++ vuejs-search-meals/src/views/MealsByName.vue
@@ -1,9 +1,28 @@
 <template>
 
+<div class="p-8">
+   <input
+   type="text"
+   class="rounded border-2 border-gray-200 w-full"
+   placeholder="Search for meals"
+   @change="searchMeals"
+ />
+</div>
+
 <div>
-   By name
+   <pre>{{ meals }}</pre>
 </div>
 </template>
 
 <script setup>
+import { computed } from '@vue/reactivity'
+import { ref } from 'vue';
+import store from '../store'
+
+const keyword = ref('')
+const meals = computed(() => store.state.searchedMeals)
+
+function searchMeals() {
+   store.dispatch('searchMeals', keyword.value)
+}
 </script>
