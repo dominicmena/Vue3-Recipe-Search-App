068d3f931dd901201c8202f58e1947d26321d6ea
dominicmena <mena.dominic@gmail.com>
2023-06-21 02:02:49 -0500

    updated styling for each recipe object

--- vuejs-search-meals/src/views/MealsByName.vue
+++ vuejs-search-meals/src/views/MealsByName.vue
@@ -10,11 +10,11 @@
  />
 </div>
 
-<div class="grid grid-cols-1 md:grid-cols-3 gap-5">
-   <div v-for="meal of meals" :key="meal.idMeal">
-      <img :src="meal.strMealThumb" alt="strMeal"/>
-      <h3>{{meal.strMeal}}</h3>
-      <div>
+<div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+   <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
+      <img :src="meal.strMealThumb" alt="strMeal" class="rounded-t-xl h-48 w-full object-cover"/>
+      <h3 class="p-3 font-semibold">{{meal.strMeal}}</h3>
+      <div class="p-3">
          <a :href="meal.strYoutube" target="_blank">Youtube</a>
          <router-link to="/">
             View
