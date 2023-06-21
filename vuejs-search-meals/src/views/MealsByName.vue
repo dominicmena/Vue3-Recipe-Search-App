6fd0da037fa64dbec5724bdb3614a8275732c342
dominicmena <mena.dominic@gmail.com>
2023-06-21 17:02:45 -0500

    added a dummy description and put the h3, description and links in a div

--- vuejs-search-meals/src/views/MealsByName.vue
+++ vuejs-search-meals/src/views/MealsByName.vue
@@ -1,38 +1,46 @@
 <template>
+  <div class="p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for meals"
+      @change="searchMeals"
+    />
+  </div>
 
-<div class="p-8">
-   <input
-   type="text"
-   v-model="keyword"
-   class="rounded border-2 border-gray-200 w-full"
-   placeholder="Search for meals"
-   @change="searchMeals"
- />
-</div>
-
-<div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-   <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
-      <img :src="meal.strMealThumb" alt="strMeal" class="rounded-t-xl h-48 w-full object-cover"/>
-      <h3 class="p-3 font-semibold">{{meal.strMeal}}</h3>
-      <div class="p-3">
-         <a :href="meal.strYoutube" target="_blank">Youtube</a>
-         <router-link to="/">
-            View
-         </router-link>
-      </div>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <img
+        :src="meal.strMealThumb"
+        alt="strMeal"
+        class="rounded-t-xl h-48 w-full object-cover"
+      />
+<div class="p-3">
+   <h3 class="p-3 font-semibold">{{ meal.strMeal }}</h3>
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolor asperiores excepturi? Animi aliquid quos, totam fugit dolores exercitationem, facilis velit ad cum expedita voluptas, omnis incidunt? Natus, ipsum odio!</p>
+   <div class="p-3">
+     <a :href="meal.strYoutube" target="_blank">Youtube</a>
+     <router-link to="/"> View </router-link>
    </div>
 </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed } from '@vue/reactivity'
-import { ref } from 'vue';
-import store from '../store'
+import { computed } from "@vue/reactivity";
+import { ref } from "vue";
+import store from "../store";
 
-const keyword = ref('')
-const meals = computed(() => store.state.searchedMeals)
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
-   store.dispatch('searchMeals', keyword.value)
+  store.dispatch("searchMeals", keyword.value);
 }
 </script>
