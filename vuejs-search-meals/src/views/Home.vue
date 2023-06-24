27459b6f10b4fdb9f461715e378894c744a36d81
dominicmena <mena.dominic@gmail.com>
2023-06-24 18:33:13 -0500

    moved letters iteration from the home page to the meals by letter view

--- vuejs-search-meals/src/views/Home.vue
+++ vuejs-search-meals/src/views/Home.vue
@@ -5,11 +5,7 @@
       class="rounded border-2 border-gray-200 w-full"
       placeholder="Search for meals"
     />
-    <div class="flex justify-center gap-2 mt-2">
-      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
-        {{ letter }}
-      </router-link>
-    </div>
+
  <pre>{{ingredients}}</pre>
   </div>
 </template>
