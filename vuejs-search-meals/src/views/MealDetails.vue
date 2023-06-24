ae80190183125e82367f60e91581e742f3c8ba42
dominicmena <mena.dominic@gmail.com>
2023-06-24 18:14:18 -0500

    removed pre tag containing all object values

--- vuejs-search-meals/src/views/MealDetails.vue
+++ vuejs-search-meals/src/views/MealDetails.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="w-[800px] mx-auto p-8">
-        <pre>{{ meal }}</pre>
         <h1 class="text-5xl font-bold mb-5">{{meal.strMeal}}</h1>
         <img :src="meal.strMealThumb" :alt="meal.strMeal"/>
         <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
