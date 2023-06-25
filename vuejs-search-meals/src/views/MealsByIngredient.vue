98d7e55f214acb0942f5a7e66eb6e19be76e8d24
dominicmena <mena.dominic@gmail.com>
2023-06-25 12:21:15 -0500

    successfully pulling data in meals by ingredients view

--- vuejs-search-meals/src/views/MealsByIngredient.vue
+++ vuejs-search-meals/src/views/MealsByIngredient.vue
@@ -1,15 +1,21 @@
 <template>
 
 <div>
-    By Ingredient
+    {{ingredients}}
 </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
+import axiosClient from '../axiosClient'
+import { ref } from 'vue';
 
+const ingredients = ref([])
 
 onMounted(() => {
-    
+    axiosClient.get('list.php?i=list').then(({data}) => {
+        debugger
+        ingredients.value = data
+    })
 })
 </script>
