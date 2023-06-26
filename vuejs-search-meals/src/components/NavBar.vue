c93cf65e1bc4671c5583310a99d5ed0fc4f8db47
dominicmena <mena.dominic@gmail.com>
2023-06-26 01:26:53 -0500

    updated home name

--- vuejs-search-meals/src/components/NavBar.vue
+++ vuejs-search-meals/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="bg-white shadow h-16 flex justify-between items-stretch">
         <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5 text-orange-500 font-bold text-[24px]">
-          Home
+          Stella Recipes
         </router-link>
         <div class="flex items-center gap-1">
           <router-link :to="{name: 'byName'}" class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white text-[20px]">
