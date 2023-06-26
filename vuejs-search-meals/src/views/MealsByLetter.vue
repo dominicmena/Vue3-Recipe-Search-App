263cdf5e07a53e399c08928245500224a150e4b8
dominicmena <mena.dominic@gmail.com>
2023-06-26 01:04:39 -0500

    added styling to meals by letters

--- vuejs-search-meals/src/views/MealsByLetter.vue
+++ vuejs-search-meals/src/views/MealsByLetter.vue
@@ -3,12 +3,12 @@
 
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals By Letter</h1>
     </div>
-    <div class="flex justify-center gap-3 px-8">
+    <div class="flex flex-wrap mb-6 justify-center gap-3 px-8">
     <router-link
       :to="{ name: 'byLetter', params: { letter } }"
       v-for="letter of letters"
       :key="letter"
-      class="hover:text-orange-500 transition-colors"
+      class="hover:text-orange-500 transition-colors w-2 h-2 flex items-center justify-center hover:scale-150"
     >
       {{ letter }}
     </router-link>
