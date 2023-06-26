0c919553d234cc9c84bd938b602802478f38902c
dominicmena <mena.dominic@gmail.com>
2023-06-26 00:12:52 -0500

    successfully added search bar in ingredients component and pulling correct items

--- vuejs-search-meals/src/views/Ingredients.vue
+++ vuejs-search-meals/src/views/Ingredients.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4"></h1>
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-full mb-3"
+      placeholder="Search for Ingredients"
+    />
     <router-link
       :to="{
         name: 'byIngredient',
         params: { ingredient: ingredient.strIngredient },
       }"
-      v-for="ingredient of ingredients"
+      v-for="ingredient of computedIngredients"
       :key="ingredient.idIngredient"
       class="block bg-white rounded p-3 mb-3 shadow"
     >
@@ -20,8 +26,14 @@
 import { onMounted } from "vue";
 import axiosClient from "../axiosClient";
 import { ref } from "vue";
+import { computed } from "vue";
 
+const keyword = ref("");
 const ingredients = ref([]);
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter((i) => i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase()));
+});
 
 onMounted(() => {
   axiosClient.get("list.php?i=list").then(({ data }) => {
