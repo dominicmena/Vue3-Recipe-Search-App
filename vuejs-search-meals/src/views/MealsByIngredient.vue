e37848fee973021cb2c04a25b9123ecc55a1966a
dominicmena <mena.dominic@gmail.com>
2023-06-25 23:31:53 -0500

    correctly rendering ingredients and pulling all recipes with relative ingredient

--- vuejs-search-meals/src/views/MealsByIngredient.vue
+++ vuejs-search-meals/src/views/MealsByIngredient.vue
@@ -1,31 +1,18 @@
 <template>
-  <div class="p-8">
-    <h1 class="text-4xl font-bold mb-4"></h1>
-    <router-link
-      :to="{
-        name: 'byIngredient',
-        params: { ingredient: ingredient.idIngredient },
-      }"
-      v-for="ingredient of ingredients"
-      :key="ingredient.idIngredient"
-      class="block bg-white rounded p-3 mb-3 shadow"
-    >
-      <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
-      <p>{{ ingredient.strDescription }}</p>
-    </router-link>
-  </div>
+<Meals :meals="meals"/>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
-import axiosClient from "../axiosClient";
-import { ref } from "vue";
+import { useRoute } from "vue-router";
+import Meals from '../components/Meals.vue'
+import store from '../store'
+import { computed } from "@vue/reactivity";
 
-const ingredients = ref([]);
+const route = useRoute()
+const meals = computed(() => store.state.mealsByIngredient)
 
 onMounted(() => {
-  axiosClient.get("list.php?i=list").then(({ data }) => {
-    ingredients.value = data.meals;
-  });
-});
+    store.dispatch('searchMealsByIngredient', route.params.ingredient)
+})
 </script>
